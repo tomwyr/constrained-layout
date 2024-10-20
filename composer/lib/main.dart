390c29e329a3eb79a5bd16713112be8076f5a474
tomwyr <tomwyr@hotmail.com>
2024-10-20 14:32:46 +0200

    Change composer window title

--- composer/lib/main.dart
+++ composer/lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'composer.dart';
 
 void main() {
+  setWindowTitle();
   runApp(const App());
+}
+
+void setWindowTitle() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  windowManager.waitUntilReadyToShow(
+    const WindowOptions(title: 'Constrained Layout'),
+  );
 }
 
 class App extends StatelessWidget {
